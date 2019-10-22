04cc712825d1b4148970e3ce4deef9dfa527cfd7
Nishant Patel <nishantpatel2718@gmail.com>
2019-10-22 08:25:58 -0400

    start onboard story

---
+++ Civit/Models/OnboardingPage.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  Civit
+//
+//  Created by Nishant Patel on 10/22/19.
+//  Copyright © 2019 nishant. All rights reserved.
+//
+
+import Foundation
+
+struct Page {
+    
+}
---
+++ Civit/Views/OnboardingPageCell.swift
@@ -0,0 +1,9 @@
+//
+//  OnboardingPageCell.swift
+//  Civit
+//
+//  Created by Nishant Patel on 10/22/19.
+//  Copyright © 2019 nishant. All rights reserved.
+//
+
+import Foundation
