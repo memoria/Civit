b1b5169c14c6a24d1d529c28d4a653dd96d38db9
Nishant Patel <nishantpatel2718@gmail.com>
2019-11-16 10:33:30 -0500

    refactored APIService to use generics

--- Civit/Service/APIService.swift
+++ Civit/Service/APIService.swift
@@ -11,44 +11,32 @@ import Foundation
 class APIService {
     static let shared = APIService() // singleton
     
-    // Change to Generics later
     func fetchGames(completion: @escaping (HomeGroup?, Error?) -> ()) {
         let urlString = "https://rss.itunes.apple.com/api/v1/us/ios-apps/new-games-we-love/all/10/explicit.json"
-        fetchHomeGroup(urlString: urlString, completion: completion)
+        fetchGenericJSONData(urlString: urlString, completion: completion)
     }
     
     func fetchTopGrossing(completion: @escaping (HomeGroup?, Error?) -> ()) {
         let urlString = "https://rss.itunes.apple.com/api/v1/us/ios-apps/top-grossing-ipad/all/25/explicit.json"
-        fetchHomeGroup(urlString: urlString, completion: completion)
+        fetchGenericJSONData(urlString: urlString, completion: completion)
     }
     
     func fetchFreeApps(completion: @escaping (HomeGroup?, Error?) -> ()) {
         let urlString = "https://rss.itunes.apple.com/api/v1/us/ios-apps/top-free/all/25/explicit.json"
-        fetchHomeGroup(urlString: urlString, completion: completion)
-    }
-    
-    // Mark: Fetch multiple categories of data
-    func fetchHomeGroup(urlString: String, completion: @escaping (HomeGroup?, Error?) -> Void) {
-        guard let url = URL(string: urlString) else { return }
-        URLSession.shared.dataTask(with: url) { (data, resp, err) in
-            do {
-                let homeGroup = try JSONDecoder().decode(HomeGroup.self, from: data!)
-                homeGroup.feed.results.forEach( {print($0.name)} )
-                completion(homeGroup, nil)
-            } catch {
-                completion(nil, error)
-                print("Failed to decode, ", error)
-            }
-        }.resume()
+        fetchGenericJSONData(urlString: urlString, completion: completion)
     }
     
     func fetchSocialApps(completion: @escaping ([SocialApp]?, Error?) -> Void) {
         let urlString = "https://api.letsbuildthatapp.com/appstore/social"
-        
+       fetchGenericJSONData(urlString: urlString, completion: completion)
+    }
+    
+    // Generic to encapsulate all cases of above
+    func fetchGenericJSONData<T: Decodable>(urlString: String, completion: @escaping (T?, Error?) -> ()) {
         guard let url = URL(string: urlString) else { return }
         URLSession.shared.dataTask(with: url) { (data, resp, err) in
             do {
-                let objects = try JSONDecoder().decode([SocialApp].self, from: data!)
+                let objects = try JSONDecoder().decode(T.self, from: data!)
                 completion(objects, nil)
             } catch {
                 completion(nil, error)
@@ -56,4 +44,5 @@ class APIService {
             }
         }.resume()
     }
+    
 }
