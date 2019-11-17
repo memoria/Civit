9db689fd76dde3dee2104ec241ef1312c500fd5e
Nishant Patel <nishantpatel2718@gmail.com>
2019-11-17 15:08:12 -0500

    keyplayers started

---
+++ Civit/VIewModel/KeyPlayers/KeyPlayersControler.swift
@@ -0,0 +1,47 @@
+//
+//  KeyPlayersControler.swift
+//  Civit
+//
+//  Created by Nishant Patel on 11/17/19.
+//  Copyright © 2019 nishant. All rights reserved.
+//
+
+import UIKit
+
+class KeyPlayersController: BaseListController, UICollectionViewDelegateFlowLayout {
+    let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.isNavigationBarHidden = true
+        collectionView.backgroundColor = #colorLiteral(red: 0.9537788033, green: 0.9487789273, blue: 0.9574493766, alpha: 1)
+        
+        collectionView.register(KeyPlayersCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! KeyPlayersCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width - 64, height: 400)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 32
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 32, left: 0, bottom: 32, right: 0)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Animate this guy fullscreen soon.")
+    }
+    
+}
