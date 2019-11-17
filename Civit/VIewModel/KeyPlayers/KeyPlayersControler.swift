3209527838f6b31f809457fb4bcb2c7dabe14e21
Nishant Patel <nishantpatel2718@gmail.com>
2019-11-17 15:08:12 -0500

    keyplayers started
    fin v29
    
    fin v30

---
+++ Civit/VIewModel/KeyPlayers/KeyPlayersControler.swift
@@ -0,0 +1,84 @@
+//
+//  KeyPlayersControler.swift
+//  Civit
+//
+//  Created by Nishant Patel on 11/17/19.
+//  Copyright © 2019 nishant. All rights reserved.
+//
+
+import UIKit
+
+class KeyPlayersController: BaseListController, UICollectionViewDelegateFlowLayout {
+    let cellId = "cellId"
+    var startingFrame: CGRect?
+    var keyPlayersFullScreenController: UIViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.isNavigationBarHidden = true
+        collectionView.backgroundColor = #colorLiteral(red: 0.9537788033, green: 0.9487789273, blue: 0.9574493766, alpha: 1)
+        
+        collectionView.register(KeyPlayersCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! KeyPlayersCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width - 64, height: 400)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 32
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 32, left: 0, bottom: 32, right: 0)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let KeyPlayersFullScreen = KeyPlayersFullScreenController()
+        let redView = KeyPlayersFullScreen.view!
+        
+        redView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleRemoveRedView)))
+        view.addSubview(redView)
+        redView.layer.cornerRadius = 16
+        addChild(KeyPlayersFullScreen)
+        self.keyPlayersFullScreenController = KeyPlayersFullScreen
+        
+        redView.frame = CGRect(x: 0, y: 0, width: 100, height: 200)
+        
+        guard let cell = collectionView.cellForItem(at: indexPath) else { return }
+        
+        guard let startingFrame = cell.superview?.convert(cell.frame, to: nil) else { return }
+        self.startingFrame = startingFrame
+        redView.frame = startingFrame
+        
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseInOut, animations: {
+            redView.frame = self.view.frame
+//            self.tabBarController?.tabBar.transform = CGAffineTransform(translationX: 0, y: 100)
+            self.tabBarController?.tabBar.frame.origin.y = self.view.frame.size.height
+        }, completion: nil)
+    }
+    
+    @objc func handleRemoveRedView(gesture: UITapGestureRecognizer) {
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseInOut, animations: {
+            gesture.view?.frame = self.startingFrame ?? .zero
+//            self.tabBarController?.tabBar.transform = .identity
+            if let tabBarFrame = self.tabBarController?.tabBar.frame {
+                self.tabBarController?.tabBar.frame.origin.y = self.view.frame.size.height - tabBarFrame.height
+            }
+            
+        }, completion: { _ in
+            gesture.view?.removeFromSuperview()
+            self.keyPlayersFullScreenController.removeFromParent()
+        })
+    }
+    
+} // end KeyPlayersController
